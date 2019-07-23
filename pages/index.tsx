3264eadb61559d9fa04dd87589532bbb6b3e7e45
Jaemin Ahn <jae7415@gmail.com>
2019-07-23 12:09:08 +0900

    Set Core Librarys for Project

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const IndexPage: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1>Boilerplate</h1>
+      <h2>Stack</h2>
+      <h3>CORE</h3>
+      <ul>
+        <li>React</li>
+        <li>Next.js</li>
+        <li>Typescript</li>
+      </ul>
+    </React.Fragment>
+  )
+}
+
+export default IndexPage;
